920b667b2c0f39974e0c38a397f48313e4f72a49
thor2301 <thor_2301@yahoo.de>
2018-09-08 01:26:33 +0200

    Update ThorArch.sh

--- ThorArch.sh
+++ ThorArch.sh
@@ -7,8 +7,7 @@ echo "################################################################"
 # installing refector to test wich servers are fastest
 sudo pacman -S --noconfirm --needed reflector
 # finding the fastest archlinux servers
-sudo reflector -l 50 -f 50 -c DE --sort rate --threads 5 --verbose --save /tmp/mirrorlist.new && rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist && sudo cp /tmp/mirrorlist /etc/pacman.d
-
+sudo reflector -l 50 -f 50 -c DE --sort rate --verbose --save /etc/pacman.d/mirrorlist
 echo "################################################################"
 echo "####       	SERVER GESPEICHERT          		   ###"
 echo "################################################################"
